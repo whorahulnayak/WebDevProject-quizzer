7b4f5e2d5ef288ba6fd98a0e4abd591c99853c7b
whorahulnayak <56409793+whorahulnayak@users.noreply.github.com>
2021-01-31 11:57:09 +0530

    home page

--- category.php
+++ category.php
@@ -91,13 +91,13 @@ session_start();
 <div class="container-fluid row mx-auto justify-content-center text-center ">
       <div class="col-sm-6 col-lg-4 col-xl-3 parent2">
         
-          <a href="#" class="cardn yellow">
+          <a href="quiz1.php" class="cardn yellow">
                <div class="overlay"></div>
             <div class="circle">
                <!-- <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24"><path d="M11.266 7l12.734-2.625-.008-.042-1.008-4.333-21.169 4.196c-1.054.209-1.815 1.134-1.815 2.207v14.597c0 1.657 1.343 3 3 3h18c1.657 0 3-1.343 3-3v-14h-12.734zm8.844-5.243l2.396 1.604-2.994.595-2.398-1.605 2.996-.594zm-5.898 1.169l2.4 1.606-2.994.595-2.401-1.607 2.995-.594zm-5.904 1.171l2.403 1.608-2.993.595-2.406-1.61 2.996-.593zm-2.555 5.903l2.039-2h3.054l-2.039 2h-3.054zm4.247 10v-7l6 3.414-6 3.586zm4.827-10h-3.054l2.039-2h3.054l-2.039 2zm6.012 0h-3.054l2.039-2h3.054l-2.039 2z"/></svg> -->
             </div>
           
-            <p>Movies</p>
+            <p>Mathematics</p>
           </a>
         
       </div>
@@ -120,7 +120,7 @@ session_start();
           <div class="circle">
           </div>
         
-          <p>TV Shows</p>
+          <p>Data structures</p>
         </a>
       </div>
       
@@ -131,7 +131,7 @@ session_start();
           <div class="circle">
           </div>
         
-          <p>Science</p>
+          <p>Movies</p>
         </a>
       </div>
      <div class="col-sm-6 col-lg-4 col-xl-3 parent2">
