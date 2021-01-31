af7efb0ded9b4759afc294384c060e5b62ddf7d5
whorahulnayak <56409793+whorahulnayak@users.noreply.github.com>
2021-01-31 21:47:24 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -30,43 +30,6 @@ session_start();
 ?>
 
 
-<!-- <!DOCTYPE html>
-<html>
-<head>
-	<title>Signup</title>
-</head>
-<body>
-
-	<style type="text/css">
-	
-	#text{
-
-		height: 25px;
-		border-radius: 5px;
-		padding: 4px;
-		border: solid thin #aaa;
-		width: 100%;
-	}
-
-	#button{
-
-		padding: 10px;
-		width: 100px;
-		color: white;
-		background-color: lightblue;
-		border: none;
-	}
-
-	#box{
-
-		background-color: grey;
-		margin: auto;
-		width: 300px;
-		padding: 20px;
-	}
-
-	</style> -->
-
 <!DOCTYPE html>
 <html lang="en">
   <head>
